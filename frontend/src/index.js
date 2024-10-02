83e6188d38d94aba7639ce85cc668c717b32b0a6
BeauC2481 <91040365+BeauC2481@users.noreply.github.com>
2024-10-02 16:04:38 +1000

    Delete frontend directory

--- frontend/src/index.js
+++
@@ -1,9 +0,0 @@
-// src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(
-  <App />, // Ensure this is the only rendered component
-  document.getElementById('root')
-);
